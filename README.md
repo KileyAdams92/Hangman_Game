0d99598988aa95976fc4c8cffa9e1f1fe11ee8f4
Kiley Adams <kileyadams@gmail.com>
2018-02-03 20:30:00 -0600

    points system added, spacing modified, picture size modified, reset button added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Hangman_Game
+
+What the project does?
+
+This project is designed to function as a simple hangman style game with three word options for guessing. The user guesses a single letter at a time, decreasing the number of attempts remaining (ranging from 20-0). The game displays letters as they are chosen on the screen, and alerts players when an incorrect key is pressed. Once a user has guessed the entire word, a picture of the corresponding ship shows up on the left column, the user can then press the reset button to reset the word to a new word. The number of wins goes up by 1 with each correct full word guessed. 
+
+How can users get started with the project?
+	
+Users can locate the project at , and get started by pressing any key.
+
+Where can users get help with the project?
+
+Users can contact Kiley on her contact page of her portfolio with any questions or problems regarding the game. 
+
+Who maintains and contributes to the project?
+	
+Kiley Adams maintains and contributes to the project. 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 
-var shipOptions = ["edmundfitzgerald", "johnbcowle", "steamervienna"]
-    
-
+var shipOptions = ["edmundfitzgerald", "johnbcowle", "steamervienna"];
+var shipPictures = ["ef.jpg", "jbc.jpeg", "sv.jpeg"];
 
 var validLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -12,54 +11,85 @@ var lettersGuessed = [];
 var randomWord;
 var blankWord;
 var blankArray;
+var randomWordLength;
+
+//created reset, reset document. to blank when game starts
+function resetButton() {
+    attemptsLeft = 20;
+    lettersGuessed = [];
+    document.querySelector("#lettersGuessed").innerHTML = "";
+    document.querySelector("#results").innerHTML = "";
+    document.getElementById("photos").innerHTML = "";
+    startGame();
+}
 function startGame() {
-    var randomNumber = Math.floor(Math.random() * 3);   
+    var randomNumber = Math.floor(Math.random() * 3);
+    // making a random word from the random index of shipOptions
     randomWord = shipOptions[randomNumber];
-    console.log(randomWord);
+    //number of letters inside of the random.word length
+    randomWordLength = randomWord.length;
     blankArray = [];
+    //set the variable at 0, keep running the loop as long as i < randomWord.length (one of the shipOptions), everytime you go through loop increase i + 1
     for (var i = 0; i < randomWord.length; i++) {
         blankArray[i] = "_";
-    } 
+    }
+    // places spaces in between underscores
     blankWord = blankArray.join(" ");
     document.querySelector("#word").innerHTML = blankWord;
+    document.getElementById("photos").innerHTML = '<img src="assets/images/glp.png' + '" />';
 }
- 
 
-document.onkeyup = function(event) {
+
+document.onkeyup = function (event) {
     var userGuess = event.key.toLowerCase();
     lettersGuessed.push(userGuess);
     var logLetters =
-    "<p> Letters already chosen : " + lettersGuessed + "</p>";
+        "<p> Letters already chosen : " + lettersGuessed + "</p>";
     document.querySelector("#lettersGuessed").innerHTML = logLetters;
-    
-    console.log(randomWord.indexOf(userGuess));
-    if(validLetters.indexOf(userGuess) === -1) {
+
+    if (validLetters.indexOf(userGuess) === -1) {
         alert("Please press a key between a-z");
     }
     else {
-        console.log("positive key press");
         attemptsLeft--;
         var logAttempts =
-        "<p> Attempts remaining : " + attemptsLeft + "</p>";
+            "<p> Attempts remaining : " + attemptsLeft + "</p>";
         document.querySelector("#results").innerHTML = logAttempts;
-        for (var i = 0; i < randomWord.length; i++) {
-            //if userGuess === index of random word
-            //replace "_" with userGuess
-            if(randomWord[i] === userGuess) {
-                // randomWord[i] = blankWord[i];
-                blankWord[i] = userGuess;
-                blankArray.splice(i, 1, userGuess);
-                console.log(blankWord);
-                blankWord = blankArray.join(" ");
-                document.querySelector("#word").innerHTML = blankWord;
-            } 
+        // if has to wrap the for loop because if the blank array has a letter we guessed already we don't need to run for loop
+        //user can't guess the same letter twice
+        if ((!blankArray.includes(userGuess))) {
+            for (var i = 0; i < randomWord.length; i++) {
+                //if userGuess === index of random word
+                //replace "_" with userGuess
+                if (randomWord[i] === userGuess) {
+                    randomWordLength--;
+                    blankWord[i] = userGuess;
+                    blankArray.splice(i, 1, userGuess);
+                    //tracking how many letters are left to guess
+                    //if blankArray does not include userGUess then decrease randomWordLength
+                    blankWord = blankArray.join(" ");
+                    document.querySelector("#word").innerHTML = blankWord;
+
+                }
+            }
+            //is the user guess inside of the blankArray
+
+            if (randomWordLength === 0) {
+                document.getElementById("photos").innerHTML = '<img src="assets/images/' + shipPictures[shipOptions.indexOf(randomWord)] + '" />';
+                points++;
+                var logPoints =
+                "<p> Points : " + points + "</p>";
+                document.querySelector("#pointsWon").innerHTML = logPoints;
+            }
+
+
             //if user guesses all letters of one word {
             //show a picture of corresponding ship
             //automatically show new word
             //}
             if (attemptsLeft === 0) {
-        alert("GAME OVER - NICE TRY!");
-    }
+                alert("GAME OVER - NICE TRY!");
+            }
         }
 
 
@@ -76,8 +106,11 @@ document.onkeyup = function(event) {
 
         // END positive condition
     }
-    
+
     // anything here will be executed on EVERY onkeyup
 
 }
+
 startGame();
+//go get element with id reset, when clicked, run this function;
+document.getElementById("reset").onclick = resetButton;
